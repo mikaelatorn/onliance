db67e2af7d2b52713f8cf5204955f3511b6b18b2
tornlund <tornlund@student.chalmers.se>
2020-10-07 09:39:08 +0200

    search design

---
+++ src/components/search/results.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-row :gutter="10" class="justify-center">
+      <el-col class="result-column" :xs="22">
+          <el-card class="card" shadow="hover"></el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10" class="justify-center">
+      <el-col class="result-column" :xs="22">
+          <el-card class="card" shadow="hover"></el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10" class="justify-center">
+      <el-col class="result-column" :xs="22">
+          <el-card class="card" shadow="hover"></el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10" class="justify-center">
+      <el-col class="result-column" :xs="22">
+          <el-card class="card" shadow="hover"></el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10" class="justify-center">
+      <el-col class="result-column" :xs="22">
+          <el-card class="card" shadow="hover"></el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SearchResults',
+  data () {
+    return {
+    }
+  }
+}
+</script>
